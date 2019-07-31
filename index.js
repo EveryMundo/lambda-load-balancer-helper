1f826224fd67c8d520bf858376f9cca74046fd7d
Daniel Santana <github@danielsantana.net>
2019-07-31 18:37:49 -0400

    add plainResponse method

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 
 const statusDescriptionMap = {
   200: 'Ok',
-  500: 'Error',
-};
+  500: 'Error'
+}
 
 const defaultHeaders = {
   'Set-cookie': 'cookies',
-  'Content-Type': 'application/json',
-};
+  'Content-Type': 'application/json'
+}
 
 const response = ({
   isBase64Encoded = false,
   statusCode,
   statusDescription = `${statusCode} ${statusDescriptionMap[statusCode]}`,
   headers = defaultHeaders,
-  body = {},
+  body = {}
 }, debug = false) => {
   if (debug) {
-    body.versions = process.versions;
+    body.versions = process.versions
   }
 
   return {
@@ -25,23 +25,44 @@ const response = ({
     statusCode,
     statusDescription,
     headers,
-    body: JSON.stringify(body),
-  };
-};
+    body: JSON.stringify(body)
+  }
+}
+
+const plainResponse = ({
+  isBase64Encoded = false,
+  statusCode,
+  statusDescription = `${statusCode} ${statusDescriptionMap[statusCode]}`,
+  headers = defaultHeaders,
+  body = '' }, debug = false) => {
+  if (debug) {
+    headers['x-node-version'] = process.version
+    headers['x-node-lib-versions'] = process.versions
+  }
+
+  return {
+    isBase64Encoded,
+    statusCode,
+    statusDescription,
+    headers,
+    body
+  }
+}
 
 const errorResponse = (message, statusCode = 500, debug = false) => response({
   statusCode,
-  body: {statusCode, message},
-}, debug);
+  body: { statusCode, message }
+}, debug)
 
-const okResponse = (data, statusCode = 200, debug = false)  => response({
+const okResponse = (data, statusCode = 200, debug = false) => response({
   statusCode,
-  body: {statusCode, data},
-}, debug);
+  body: { statusCode, data }
+}, debug)
 
 module.exports = {
   defaultHeaders,
   errorResponse,
   okResponse,
   response,
-};
+  plainResponse
+}
